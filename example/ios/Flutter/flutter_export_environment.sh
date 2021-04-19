954764c6ee5ad0616ea5e0aeecdb0cf0b8cc22e3
Shinji Sugita <sugita@qrestia.com>
2021-04-19 17:01:53 +0900

    build

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/iirokrankka/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/iirokrankka/flutter_facebook_login/example"
-export "FLUTTER_TARGET=/Users/iirokrankka/flutter_facebook_login/example/lib/main.dart"
+export "FLUTTER_ROOT=/Users/sugitashinji/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/sugitashinji/repos/flutter_facebook_login/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/iirokrankka/flutter/bin/cache/artifacts/engine/ios"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_FRAMEWORK_DIR=/Users/sugitashinji/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
